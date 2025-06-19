56f72e5b5aca36a98b71145c6ee087989a8ad7a0
Panyaphon02 <arma04425@gmail.com>
2025-06-19 07:36:18 +0000

    ze

--- miniproject/mini.py
+++ miniproject/mini.py
@@ -12,7 +12,7 @@ def show_menu():
     print("5. ออกจากโปรแกรม")
 
 def add_task():
-    name = input("ชื่องาน: ")
+    name = input("(\U0001F4CC)ชื่องาน: ")
     category = input("ประเภท (พืช,สัตว์,อื่นๆ): ")
     category = input("ป้อนวันที่ : ")
     tasks.append({"name": name, "category": category})
