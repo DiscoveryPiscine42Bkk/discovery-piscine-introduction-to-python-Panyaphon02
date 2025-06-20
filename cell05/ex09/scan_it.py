aa05211edd8f6c127d95486765a1b7c64edd0519
Panyaphon02 <arma04425@gmail.com>
2025-06-20 03:37:58 +0000

    axd

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,1 +1,13 @@
-print("RTFM (Read the F-ing manual)")
+import sys
+
+def main():
+    if len(sys.argv) < 3:
+        print("none")
+    else:
+        word = sys.argv[1]
+        text = sys.argv[2]
+        count = text.split().count(word)
+        print(count)
+
+if __name__ == "__main__":
+    main()
