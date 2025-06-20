3e921f6cdec7c98eb03cd402df8093bbdf516399
Panyaphon02 <arma04425@gmail.com>
2025-06-20 03:43:24 +0000

    yhyjuurs

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -1,1 +1,13 @@
-print("RTFM (Read the F-ing manual)")
+import sys
+
+def main():
+    args = sys.argv[1:]
+    if not args:
+        print("none")
+    else:
+        print(f"parameters: {len(args)}")
+        for arg in args:
+            print(f"{arg}: {len(arg)}")
+
+if __name__ == "__main__":
+    main()
