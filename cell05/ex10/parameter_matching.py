b4cabd03fbd2f0cd91de90aa6d16c129ee1c2212
Panyaphon02 <arma04425@gmail.com>
2025-06-20 03:40:34 +0000

    zdv

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,1 +1,15 @@
-print("RTFM (Read the F-ing manual)")
+import sys
+
+def main():
+    if len(sys.argv) < 2:
+        print("none")
+    else:
+        expected = sys.argv[1]
+        user_input = input("What was the parameter? ")
+        if user_input == expected:
+            print("Good job!")
+        else:
+            print("Nope, sorry...")
+
+if __name__ == "__main__":
+    main()
